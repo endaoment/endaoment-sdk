30153b6ad6397573bfc1351b49162bc6cbfec01b
Ricardo Pedroni <ricardo@endaoment.org>
2023-01-27 14:55:25 -0300

    pretty up example

--- examples/ui-search/src/App.tsx
+++ examples/ui-search/src/App.tsx
@@ -1,6 +1,7 @@
-import { Configuration, EndaomentSdkApi, OrgDto } from '@endaoment/sdk';
+import { Configuration, EndaomentSdkApi, FundDto, OrgDto } from '@endaoment/sdk';
 
 import { useState } from 'react';
+import parse from 'html-react-parser';
 import {
   Container,
   Input,
@@ -18,26 +19,32 @@ import {
   AccordionIcon,
   Tag,
   Link,
+  Image,
+  Avatar,
 } from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 
-const config = new Configuration({ network: 'goerli' });
+const config = new Configuration({ network: 'local' });
 const sdk = new EndaomentSdkApi(config);
 
 function App() {
   const [search, setSearch] = useState('');
   const [loading, setLoading] = useState(false);
-  const [searchResultOrgs, setSearchResultOrgs] = useState<OrgDto[]>();
+  const [searchedEntities, setSearchedEntities] = useState<(FundDto | OrgDto)[]>();
 
   const handleSearch = async () => {
     setLoading(true);
-    const orgs = await sdk.searchDeployedOrgs({ name: search });
-    setSearchResultOrgs(orgs);
+    const [funds, orgs] = await Promise.all([
+      sdk.searchVisibleFunds({ name: search }),
+      sdk.searchDeployedOrgs({ name: search }),
+    ]);
+    setSearchedEntities([...funds, ...orgs]);
     setLoading(false);
     setSearch('');
   };
 
   return (
-    <Container p={16}>
+    <Container maxW="2xl" p="4" mt="16">
       <Flex alignItems="center" gap="2">
         <Input onChange={(e) => setSearch(e.target.value)} value={search} placeholder="Search Endaoment" />
 
@@ -51,30 +58,38 @@ function App() {
       <Divider margin={4} />
 
       <Flex direction={'column'} gap="2" padding={5}>
-        {searchResultOrgs?.length === 0 && <Stat>No results found</Stat>}
+        {searchedEntities?.length === 0 && <Stat>No results found</Stat>}
         <Accordion>
-          {searchResultOrgs?.map((org) => (
-            <AccordionItem>
+          {searchedEntities?.map((entity) => (
+            <AccordionItem key={entity.id}>
               <h2>
                 <AccordionButton p="4">
                   <Box flex="1" textAlign="left">
-                    <Heading size="sm">{org.name}</Heading>
+                    <Heading size="sm">{entity.name}</Heading>
                   </Box>
                   <AccordionIcon />
                 </AccordionButton>
               </h2>
               <AccordionPanel>
-                <p>{org.description}</p>
-                <Box p="2" mt="2">
-                  Org Contract{' '}
-                  <Link href={`https://etherscan.io/address/${org.contractAddress}`}>
+                <Flex alignItems="center" gap="8" p="4">
+                  {entity.logoUrl && <Avatar src={entity.logoUrl?.toString()} name={entity.name} size="2xl" />}
+                  <p>{parse(entity.description || '')}</p>
+                </Flex>
+
+                <Flex mt="2" justifyContent="space-between">
+                  <span>
+                    Contract{' '}
                     <Tag>
-                      {org.contractAddress.slice(0, 5)}...{org.contractAddress.slice(-3)}
+                      {entity.contractAddress.slice(0, 5)}...{entity.contractAddress.slice(-3)}
                     </Tag>
+                    <Link href={`https://etherscan.io/address/${entity.contractAddress}`} target="_blank" ml="1">
+                      <ExternalLinkIcon />
+                    </Link>
+                  </span>
+                  <Link href={entity.endaomentUrl} target="_blank">
+                    View on Endaoment
                   </Link>
-                  <br />
-                  <span>View on Endaoment</span>
-                </Box>
+                </Flex>
               </AccordionPanel>
             </AccordionItem>
           ))}

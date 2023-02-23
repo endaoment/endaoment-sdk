586b108fe5e9d8f2f6d5d49d7b7a6be85db572e5
Ricardo Pedroni <ricardo@endaoment.org>
2023-02-23 16:36:28 -0500

    chore: use mainnet, update for demo

--- examples/src/index.tsx
+++ examples/src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { goerli, foundry } from 'wagmi/chains';
+import { mainnet, foundry } from 'wagmi/chains';
 import { ConnectKitProvider, getDefaultClient } from 'connectkit';
 import { publicProvider } from 'wagmi/providers/public';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 
-const { chains } = configureChains([goerli, foundry], [publicProvider()]);
+const { chains } = configureChains([mainnet, foundry], [publicProvider()]);
 
 const client = createClient(getDefaultClient({ appName: 'Endaoment SDK Examples', chains }));
 

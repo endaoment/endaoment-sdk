04b228347dc8a75fe5154aef37e7d015d754175e
Ishaan <ishbharal@gmail.com>
2023-02-27 20:08:37 -0600

    config oopsies

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -5,4 +5,7 @@ import viteTsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths()],
+  server: {
+    port: 3000,
+  },
 });

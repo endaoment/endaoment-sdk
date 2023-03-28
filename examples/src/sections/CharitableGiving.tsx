a7023e003655e7685f90eb4119ffd3869b976188
Ricardo Pedroni <ricardo@endaoment.org>
2023-03-28 12:39:12 -0300

    improve donation section

--- examples/src/sections/CharitableGiving.tsx
+++ examples/src/sections/CharitableGiving.tsx
@@ -17,8 +17,19 @@ import {
   InputLeftAddon,
 } from '@chakra-ui/react';
 import { ChevronRightIcon, InfoIcon } from '@chakra-ui/icons';
-import { usePrepareSendTransaction, useSendTransaction } from 'wagmi';
+import {
+  usePrepareSendTransaction,
+  useSendTransaction,
+  useContractRead,
+  erc20ABI,
+  Address,
+  useAccount,
+  usePrepareContractWrite,
+  useContractWrite,
+} from 'wagmi';
 import NumberFormatter from 'react-currency-format';
+import { parseUnits } from 'ethers/lib/utils.js';
+import { BigNumber } from 'ethers';
 
 // Forked mainnet and goerli addresses
 const TOKENS = [
@@ -27,58 +38,106 @@ const TOKENS = [
     decimals: 18,
     contractAddress: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
   },
-  // {
-  //   symbol: 'WETH',
-  //   decimals: 18,
-  //   contractAddress: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-  // },
-  // {
-  //   symbol: 'USDC',
-  //   decimals: 6,
-  //   contractAddress: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-  // },
-  // {
-  //   symbol: 'LINK',
-  //   decimals: 18,
-  //   contractAddress: '0x514910771AF9Ca656af840dff83E8264EcF986CA',
-  // },
+  {
+    symbol: 'WETH',
+    decimals: 18,
+    contractAddress: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+  },
+  {
+    symbol: 'USDC',
+    decimals: 6,
+    contractAddress: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  },
+  {
+    symbol: 'DAI',
+    decimals: 18,
+    contractAddress: '0x6b175474e89094c44da98b954eedeac495271d0f',
+  },
 ];
 
 function CharitableGiving({ sdk }: { sdk: EndaomentSdkApi }) {
+  const { address } = useAccount();
+
   const [ein, setEin] = useState('844661797');
   const [selectedToken, setSelectedToken] = useState(TOKENS[0]);
   const [amountIn, setAmountIn] = useState('1');
-  const [exp, setExp] = useState(1e18);
+  const [amountInParsed, setAmountInParsed] = useState(parseUnits(amountIn || '0', selectedToken.decimals));
   const [loading, setLoading] = useState(false);
   const [swapAndDonateTransaction, setSwapAndDonateTransaction] = useState<NdaoSdkDonationSwap>();
-  const { config, error } = usePrepareSendTransaction({
+
+  // Send swap and donate transaction
+  const { config: config1 } = usePrepareSendTransaction({
     request: {
       to: swapAndDonateTransaction?.to as string,
       data: swapAndDonateTransaction?.data as string,
       value: swapAndDonateTransaction?.value as string,
-      gasLimit: 500_000,
+      gasLimit: 1_000_000,
     },
+    enabled: !!swapAndDonateTransaction,
     onError: (error) => console.error(error),
   });
-  const { sendTransaction } = useSendTransaction(config);
+  const { sendTransaction } = useSendTransaction(config1);
+
+  const isETH = selectedToken.symbol === 'ETH';
+
+  const { data: entityAllowance } = useContractRead({
+    address: selectedToken.contractAddress as Address,
+    abi: erc20ABI,
+    functionName: 'allowance',
+    args: [address as Address, swapAndDonateTransaction?.to as Address],
+    enabled: !isETH && !!address && !!swapAndDonateTransaction?.to,
+    watch: true,
+  });
+
+  const { config: config2 } = usePrepareContractWrite({
+    address: selectedToken.contractAddress as Address,
+    abi: erc20ABI,
+    functionName: 'approve',
+    args: [swapAndDonateTransaction?.to as Address, amountInParsed],
+    enabled: !isETH && !!address && !!swapAndDonateTransaction?.to,
+    overrides: {
+      gasLimit: BigNumber.from(100_000),
+    },
+  });
+  const { write: erc20Approve } = useContractWrite(config2);
+
+  const requiresAllowance = entityAllowance && entityAllowance.lt(amountInParsed);
+
+  console.log(amountIn, amountInParsed, entityAllowance, requiresAllowance);
+
+  const handleTokenChange = (v: any) => {
+    setSwapAndDonateTransaction(undefined);
+    setSelectedToken(TOKENS.find((t) => t.contractAddress === v.target.value)!);
+  };
+
+  const handleAmountInChange = (v: string) => {
+    if (isNaN(+v)) return;
+    setSwapAndDonateTransaction(undefined);
+
+    setAmountIn(v);
+    setAmountInParsed(parseUnits(v || '0', selectedToken.decimals));
+  };
+
+  const handleApproveAllowance = () => {
+    if (requiresAllowance && erc20Approve) erc20Approve();
+  };
 
   const handleGetTransactionData = async () => {
     setLoading(true);
-    setSwapAndDonateTransaction({
-      ...(await sdk.getDonationSwapTransaction({
-        ein,
-        tokenContractAddress: selectedToken.contractAddress,
-        amountIn: `${+amountIn * +exp}`,
-      })),
+
+    const tx = await sdk.getDonationSwapTransaction({
+      ein,
+      tokenContractAddress: selectedToken.contractAddress,
+      amountIn: amountInParsed.toString(),
     });
+    setSwapAndDonateTransaction(tx);
+
     setLoading(false);
   };
 
   const handleSwapAndDonate = async () => {
     if (swapAndDonateTransaction && sendTransaction) sendTransaction();
     else console.error('No deploy transaction or sendTransaction function');
-
-    console.log(swapAndDonateTransaction, sendTransaction, error);
   };
 
   return (
@@ -97,21 +156,10 @@ function CharitableGiving({ sdk }: { sdk: EndaomentSdkApi }) {
             </NumberInput>
           </InputGroup>
 
-          <Select
-            value={selectedToken.contractAddress}
-            onChange={(v) => setSelectedToken(TOKENS.find((t) => t.contractAddress === v.target.value)!)}
-          >
-            {TOKENS.map((token) => (
-              <option key={token.contractAddress} value={token.contractAddress}>
-                {token.symbol}
-              </option>
-            ))}
-          </Select>
-
           <Flex gap="2" w="100%">
             <NumberInput
               flex="3"
-              onChange={(v) => setAmountIn(v)}
+              onChange={handleAmountInChange}
               value={amountIn}
               onKeyDown={(e) => e.key === 'Enter' && handleGetTransactionData()}
             >
@@ -120,12 +168,12 @@ function CharitableGiving({ sdk }: { sdk: EndaomentSdkApi }) {
               />
             </NumberInput>
 
-            <Select flex="1" value={exp} onChange={(v) => setExp(+v.target.value)}>
-              <option value={1e18}>10^18</option>
-              <option value={1e9}>10^9</option>
-              <option value={1e6}>10^6</option>
-              <option value={1e3}>10^3</option>
-              <option value={1}>1</option>
+            <Select value={selectedToken.contractAddress} onChange={handleTokenChange} flex="1">
+              {TOKENS.map((token) => (
+                <option key={token.contractAddress} value={token.contractAddress}>
+                  {token.symbol}
+                </option>
+              ))}
             </Select>
           </Flex>
         </VStack>
@@ -200,14 +248,26 @@ function CharitableGiving({ sdk }: { sdk: EndaomentSdkApi }) {
               </ListItem>
             </UnorderedList>
 
-            <Button
-              onClick={handleSwapAndDonate}
-              rightIcon={<ChevronRightIcon />}
-              aria-label="Deploy Org"
-              colorScheme="green"
-            >
-              Swap & Donate
-            </Button>
+            {requiresAllowance && (
+              <Button
+                onClick={handleApproveAllowance}
+                rightIcon={<ChevronRightIcon />}
+                aria-label="Approve Allowance"
+                colorScheme="blue"
+              >
+                Approve Allowance
+              </Button>
+            )}
+            {!requiresAllowance && (
+              <Button
+                onClick={handleSwapAndDonate}
+                rightIcon={<ChevronRightIcon />}
+                aria-label="Swap and Donate"
+                colorScheme="green"
+              >
+                Swap & Donate
+              </Button>
+            )}
           </Flex>
         )}
       </VStack>
